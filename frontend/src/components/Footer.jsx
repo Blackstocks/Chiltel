5b220959728af0a82e3ad2da97d3a2bf8a11bc48
Ankit Kumar Soni <blackstocks@Ankits-MacBook-Air.local>
2024-11-15 06:32:10 +0530

    Collection added2

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-white text-black py-8">
-      <div className="container mx-auto flex flex-wrap md:flex-nowrap justify-between px-4 md:px-20 gap-8">
+    <footer className="py-8 text-black bg-white">
+      <div className="container flex flex-wrap justify-between gap-8 px-4 mx-auto md:flex-nowrap md:px-20">
         {/* About Company Section */}
-        <div className="w-full md:w-1/3 mb-8 md:mb-0">
-          <h3 className="text-lg font-semibold mb-4">ABOUT COMPANY</h3>
-          <p className="text-gray-600 mb-5">
+        <div className="w-full mb-8 md:w-1/3 md:mb-0">
+          <h3 className="mb-4 text-lg font-semibold">ABOUT COMPANY</h3>
+          <p className="mb-5 text-gray-600">
             Chiltel India, founded by Mr. Sudarshan Kuumar Raut in 2021, is a
             recognized startup in the Home Appliances Sales and Service sector,
             earning DIIPT recognition from the Indian government in 2022.
@@ -15,40 +15,40 @@ const Footer = () => {
           <div className="flex space-x-4">
             <a
               href="#"
-              className="bg-gray-200 p-2 rounded-full hover:bg-gray-300"
+              className="p-2 bg-gray-200 rounded-full hover:bg-gray-300"
             >
               <img
-                src="src/assets/google.png"
+                src="assets/google.png"
                 alt="Google"
                 className="w-5 h-5"
               />
             </a>
             <a
               href="#"
-              className="bg-gray-200 p-2 rounded-full hover:bg-gray-300"
+              className="p-2 bg-gray-200 rounded-full hover:bg-gray-300"
             >
               <img
-                src="src/assets/facebook.png"
+                src="assets/facebook.png"
                 alt="Facebook"
                 className="w-5 h-5"
               />
             </a>
             <a
               href="#"
-              className="bg-gray-200 p-2 rounded-full hover:bg-gray-300"
+              className="p-2 bg-gray-200 rounded-full hover:bg-gray-300"
             >
               <img
-                src="src/assets/instagram.png"
+                src="assets/instagram.png"
                 alt="Instagram"
                 className="w-5 h-5"
               />
             </a>
             <a
               href="#"
-              className="bg-gray-200 p-2 rounded-full hover:bg-gray-300"
+              className="p-2 bg-gray-200 rounded-full hover:bg-gray-300"
             >
               <img
-                src="src/assets/linkedin.png"
+                src="assets/linkedin.png"
                 alt="LinkedIn"
                 className="w-5 h-5"
               />
@@ -57,8 +57,8 @@ const Footer = () => {
         </div>
 
         {/* Quick Links Section */}
-        <div className="w-full md:w-1/3 mb-8 md:mb-0 md:ml-8">
-          <h3 className="text-lg font-semibold mb-4">QUICK LINKS</h3>
+        <div className="w-full mb-8 md:w-1/3 md:mb-0 md:ml-8">
+          <h3 className="mb-4 text-lg font-semibold">QUICK LINKS</h3>
           <ul className="space-y-2">
             <li>
               <a href="#" className="text-gray-600 hover:text-black">
@@ -78,7 +78,7 @@ const Footer = () => {
             <li>
               <a
                 href="#"
-                className="text-gray-600 hover:text-black flex items-center"
+                className="flex items-center text-gray-600 hover:text-black"
               >
                 Privacy Policy
               </a>
@@ -86,7 +86,7 @@ const Footer = () => {
             <li>
               <a
                 href="#"
-                className="text-gray-600 hover:text-black flex items-center"
+                className="flex items-center text-gray-600 hover:text-black"
               >
                 Terms & Conditions
               </a>
@@ -94,7 +94,7 @@ const Footer = () => {
             <li>
               <a
                 href="#"
-                className="text-gray-600 hover:text-black flex items-center"
+                className="flex items-center text-gray-600 hover:text-black"
               >
                 <span className="mr-2">&#128101;</span> Partners Sign Up
               </a>
@@ -102,7 +102,7 @@ const Footer = () => {
             <li>
               <a
                 href="#"
-                className="text-gray-600 hover:text-black flex items-center"
+                className="flex items-center text-gray-600 hover:text-black"
               >
                 <span className="mr-2">&#128100;</span> Partners Login
               </a>
@@ -112,17 +112,17 @@ const Footer = () => {
 
         {/* Search and Contact Section */}
         <div className="w-full md:w-1/3">
-          <h3 className="text-lg font-semibold mb-4">CONTACTS</h3>
-          <p className="text-gray-600 mb-2">
+          <h3 className="mb-4 text-lg font-semibold">CONTACTS</h3>
+          <p className="mb-2 text-gray-600">
             &#127968; Shanti Apartment, Flat No - 1 D/2, Nowbhanga, SEC-IV,
             North 24 Parganas, Kolkata - 700105
           </p>
-          <p className="text-gray-600 mb-2">&#128231; info@chiltel.com</p>
-          <p className="text-gray-600 mb-2">&#128222; +91 70033 26830</p>
+          <p className="mb-2 text-gray-600">&#128231; info@chiltel.com</p>
+          <p className="mb-2 text-gray-600">&#128222; +91 70033 26830</p>
         </div>
       </div>
-      <div className="text-center mt-8 text-gray-500">
-        <div className="flex flex-col md:flex-row justify-evenly items-center mx-4 md:mx-8 space-y-2 md:space-y-0 md:space-x-4">
+      <div className="mt-8 text-center text-gray-500">
+        <div className="flex flex-col items-center mx-4 space-y-2 md:flex-row justify-evenly md:mx-8 md:space-y-0 md:space-x-4">
           <p className="md:ml-5">
             &copy; 2024 Copyright: Chiltel partnered with DevLaunch. All Rights
             Reserved.
