804853a6184e5f676cded0ce8576692a0dd30d52
AdityaRaj212 <aditya.education212@gmail.com>
2024-12-18 01:58:44 +0530

    made add to cart functional

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,8 @@ import jwt from 'jsonwebtoken'
 
 const authUser = async (req, res, next) => {
 
-    const { token } = req.headers;
+    // const { token } = req.headers;
+    const token = req.headers.authorization?.split(' ')[1];
 
     if (!token) {
         return res.json({ success: false, message: 'Not Authorized Login Again' })
