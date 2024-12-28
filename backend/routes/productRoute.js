37e6af8131e4eb81f65b5c680a8f9a37f22cc4b7
Bipin <bk3284488@gmail.com>
2024-12-28 15:12:39 +0530

    add route updated

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,14 +1,20 @@
-import express from 'express'
-import {addProduct, removeProduct, getProduct, listProducts, updateProduct} from '../controllers/productController.js'
-import upload from '../middleware/multer.js';
-import adminAuth from '../middleware/adminAuth.js';
+import express from "express";
+import {
+	addProduct,
+	removeProduct,
+	getProduct,
+	listProducts,
+	updateProduct,
+} from "../controllers/productController.js";
+import upload from "../middleware/multer.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter = express.Router();
 
-productRouter.post('/',adminAuth, addProduct);
-productRouter.delete('/remove/:id',adminAuth, removeProduct);
-productRouter.put('/update/:id',adminAuth, updateProduct);
-productRouter.get('/single', getProduct);
-productRouter.get('/list', listProducts);
+productRouter.post("/add", adminAuth, addProduct);
+productRouter.delete("/remove/:id", adminAuth, removeProduct);
+productRouter.put("/update/:id", adminAuth, updateProduct);
+productRouter.get("/single", getProduct);
+productRouter.get("/list", listProducts);
 
-export default productRouter
+export default productRouter;
