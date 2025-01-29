516f21fb9875ff7dbf4b99b1c12dbfe393c7af3d
blackstocks <ankitkumarsoni656.iitkha@gmail.com>
2025-01-30 02:39:09 +0530

    apk file

--- rider/src/App.jsx
+++ rider/src/App.jsx
@@ -7,6 +7,7 @@ import { useEffect } from "react";
 import { useAuth } from "./context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import LocationTracker from "./components/LocationTracker";
+import RegisterServiceWorker from "./lib/pwa/RegisterServiceWorker";
 
 const ProtectedRoute = ({ children }) => {
 	const { state } = useAuth();
